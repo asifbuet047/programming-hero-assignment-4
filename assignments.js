72f5b05d86366fb10015f4cb64a087847084d125
asifbuet047 <asifbuet047@gmail.com>
2023-08-09 17:29:06 +0600

    first commit

---
+++ assignments.js
@@ -0,0 +1,88 @@
+function cubeNumber(number) {
+    if ((typeof number) == "number") {
+        return Math.pow(number, 3);
+    } else {
+        return 'This is not a number';
+    }
+}
+
+
+
+function matchFinder(string1, string2) {
+    if ((typeof string1 == "string") && (typeof string2 == "string")) {
+        var lengthOfFirstString = string1.length;
+        var lengthOfSecondString = string2.length;
+        for (var i = 0; i < lengthOfFirstString; i++) {
+            for (var j = 0; j < lengthOfSecondString; j++) {
+                if (string1[i + j] == string2[j]) {
+                    if (j == (lengthOfSecondString - 1)) {
+                        return true;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (i == (lengthOfFirstString - 1)) {
+                return false;
+            }
+        }
+    } else {
+        return "Input is not string";
+    }
+}
+
+
+function sortMaker(arr) {
+    if (arr[0] >= 0) {
+        if (arr[1] >= 0) {
+            if (arr[0] > arr[1]) {
+                return arr;
+            } else if (arr[0] == arr[1]) {
+                return "equal";
+            } else {
+                var temp = arr[0];
+                arr[0] = arr[1];
+                arr[1] = temp;
+                return arr;
+            }
+        } else {
+            return 'Invalid Input';
+        }
+
+    } else {
+        return 'Invalid Input';
+    }
+}
+
+
+function findAddress(obj) {
+    var street = obj.street;
+    var house = obj.house;
+    var society = obj.society;
+
+    if ((typeof street) == 'undefined') {
+        street = '__';
+    }
+
+    if ((typeof house) == 'undefined') {
+        house = '__';
+    }
+
+    if ((typeof society) == 'undefined') {
+        society = '__';
+    }
+
+    var result = street + ',' + house + ',' + society;
+    return result;
+}
+
+
+let address = {
+    street: 10,
+    house: "15A",
+    society: 'Earth Perfect'
+};
+
+
